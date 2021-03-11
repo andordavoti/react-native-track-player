83953e0958040bac6a670870deaf946ba46ad91d
Andor Davoti <andor.davoti@gmail.com>
2021-03-11 18:53:12 +0100

    added typescript definition for setRepeatMode

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,8 @@
-import { Component } from 'react';
+import { Component } from "react";
 
 export = RNTrackPlayer;
 
 declare namespace RNTrackPlayer {
-
   export type EventType =
     | "playback-state"
     | "playback-error"
@@ -27,11 +26,7 @@ declare namespace RNTrackPlayer {
     | "remote-dislike"
     | "remote-bookmark";
 
-  export type TrackType =
-    | "default"
-    | "dash"
-    | "hls"
-    | "smoothstreaming";
+  export type TrackType = "default" | "dash" | "hls" | "smoothstreaming";
 
   type ResourceObject = any;
 
@@ -44,10 +39,15 @@ declare namespace RNTrackPlayer {
   export function registerEventHandler(handler: EventHandler): void;
 
   type ServiceHandler = () => Promise<void>;
-  export function registerPlaybackService(serviceFactory: () => ServiceHandler): void;
+  export function registerPlaybackService(
+    serviceFactory: () => ServiceHandler
+  ): void;
 
-  type EmitterSubscription = { remove: () => void; };
-  export function addEventListener(type: EventType, listener: (data: any) => void): EmitterSubscription;
+  type EmitterSubscription = { remove: () => void };
+  export function addEventListener(
+    type: EventType,
+    listener: (data: any) => void
+  ): EmitterSubscription;
 
   export interface TrackMetadata {
     duration?: number;
@@ -77,18 +77,41 @@ declare namespace RNTrackPlayer {
     playBuffer?: number;
     backBuffer?: number;
     maxCacheSize?: number;
-    iosCategory?: 'playback' | 'playAndRecord' | 'multiRoute' | 'ambient' | 'soloAmbient' | 'record';
-    iosCategoryMode?: 'default' | 'gameChat' | 'measurement' | 'moviePlayback' | 'spokenAudio' | 'videoChat' | 'videoRecording' | 'voiceChat' | 'voicePrompt';
-    iosCategoryOptions?: Array<'mixWithOthers' | 'duckOthers' | 'interruptSpokenAudioAndMixWithOthers' | 'allowBluetooth' | 'allowBluetoothA2DP' | 'allowAirPlay' | 'defaultToSpeaker'>;
+    iosCategory?:
+      | "playback"
+      | "playAndRecord"
+      | "multiRoute"
+      | "ambient"
+      | "soloAmbient"
+      | "record";
+    iosCategoryMode?:
+      | "default"
+      | "gameChat"
+      | "measurement"
+      | "moviePlayback"
+      | "spokenAudio"
+      | "videoChat"
+      | "videoRecording"
+      | "voiceChat"
+      | "voicePrompt";
+    iosCategoryOptions?: Array<
+      | "mixWithOthers"
+      | "duckOthers"
+      | "interruptSpokenAudioAndMixWithOthers"
+      | "allowBluetooth"
+      | "allowBluetoothA2DP"
+      | "allowAirPlay"
+      | "defaultToSpeaker"
+    >;
     waitForBuffer?: boolean;
   }
 
   interface FeedbackOptions {
     /** Marks wether the option should be marked as active or "done" */
-    isActive: boolean
+    isActive: boolean;
 
     /** The title to give the action (relevant for iOS) */
-    title: string
+    title: string;
   }
 
   export interface MetadataOptions {
@@ -122,7 +145,10 @@ declare namespace RNTrackPlayer {
 
   // Player Queue Commands
 
-  export function add(tracks: Track | Track[], insertBeforeId?: string): Promise<void>;
+  export function add(
+    tracks: Track | Track[],
+    insertBeforeId?: string
+  ): Promise<void>;
   export function remove(trackIds: string | string[]): Promise<void>;
   export function skip(trackId: string): Promise<void>;
   export function skipToNext(): Promise<void>;
@@ -131,7 +157,10 @@ declare namespace RNTrackPlayer {
 
   // Control Center / Notification Metadata Commands
   export function updateOptions(options: MetadataOptions): Promise<void>;
-  export function updateMetadataForTrack(id: string, metadata: TrackMetadata) : Promise<void>;
+  export function updateMetadataForTrack(
+    id: string,
+    metadata: TrackMetadata
+  ): Promise<void>;
 
   // Player Playback Commands
 
@@ -142,6 +171,7 @@ declare namespace RNTrackPlayer {
   export function seekTo(seconds: number): Promise<void>;
   export function setVolume(level: number): Promise<void>;
   export function setRate(rate: number): Promise<void>;
+  export function setRepeatMode(mode: boolean): Promise<void>;
 
   // Player Getters
 
@@ -163,7 +193,10 @@ declare namespace RNTrackPlayer {
     duration: number;
   }
 
-  export class ProgressComponent<P = {}, S = {}> extends Component<P, ProgressComponentState & S> {
+  export class ProgressComponent<P = {}, S = {}> extends Component<
+    P,
+    ProgressComponentState & S
+  > {
     public getProgress: () => number;
     public getBufferedProgress: () => number;
   }
@@ -203,7 +236,7 @@ declare namespace RNTrackPlayer {
   export const PITCH_ALGORITHM_LINEAR: PitchAlgorithm;
   export const PITCH_ALGORITHM_MUSIC: PitchAlgorithm;
   export const PITCH_ALGORITHM_VOICE: PitchAlgorithm;
-  
+
   export const TrackPlayerEvents: {
     REMOTE_PLAY: EventType;
     REMOTE_PLAY_ID: EventType;
@@ -239,6 +272,6 @@ declare namespace RNTrackPlayer {
   export function usePlaybackStateIs(...states: State[]): boolean;
   export function useTrackPlayerProgress(
     interval?: number,
-    pollTrackPlayerStates?: State[],
+    pollTrackPlayerStates?: State[]
   ): ProgressComponentState;
 }
